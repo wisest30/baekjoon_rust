dd758a5f7a8f8172fc70bc329e7aa0f7f5ead8e7
Harry <wisest303@gmail.com>
2022-08-21 09:05:22 +0900

    increase stack size

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!bin/bash
-time cargo run < input.txt > output.txt
+ulimit -s unlimited
+
+rustc -C 'link-args=-Wl,-stack_size,0x20000000' ./src/main.rs
+time ./main < input.txt > output.txt
+
+#time cargo run < input.txt > output.txt
