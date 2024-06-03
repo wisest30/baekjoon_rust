876802deaae92d26111c9937dd1bb4b44d69edba
HyoSeong <mongsiry013@naver.com>
2024-06-03 19:49:26 +0900

    update graham_scan

--- tests/graham_scan.rs
+++ tests/graham_scan.rs
@@ -1,15 +1,6 @@
 type T = i64;
-pub fn graham_scan(points: &Vec<(T, T)>) -> Vec<(T, T)> {
-    use std::collections::HashSet;
-
-    fn angle((x0, y0): (T, T), (x1, y1): (T, T)) -> f64 {
-        assert_ne!((x0, y0), (x1, y1));
-
-        let x = (x1 - x0) as f64;
-        let y = (y1 - y0) as f64;
-
-        y.atan2(x)
-    }
+pub fn graham_scan(mut v: Vec<(T, T)>) -> Vec<(T, T)> {
+    use std::cmp::Ordering;
 
     fn ccw((x0, y0): (T, T), (x1, y1): (T, T)) -> i32 {
         let d = x0 * y1 - x1 * y0;
@@ -22,16 +13,10 @@ pub fn graham_scan(points: &Vec<(T, T)>) -> Vec<(T, T)> {
         }
     }
 
-    // make unique points
-    let mut v = points
-        .iter()
-        .cloned()
-        .collect::<HashSet<(T, T)>>()
-        .into_iter()
-        .collect::<Vec<_>>();
+    v.sort_unstable();
+    v.dedup();
 
     if v.len() <= 2 {
-        v.sort_unstable();
         return v;
     }
 
@@ -40,15 +25,16 @@ pub fn graham_scan(points: &Vec<(T, T)>) -> Vec<(T, T)> {
 
     let (x0, y0) = v[0];
     v[1..].sort_unstable_by(|(x1, y1), (x2, y2)| {
-        let a1 = angle((x0, y0), (*x1, *y1));
-        let a2 = angle((x0, y0), (*x2, *y2));
-
-        if a1 == a2 {
-            let d1 = (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0);
-            let d2 = (x2 - x0) * (x2 - x0) + (y2 - y0) * (y2 - y0);
-            d1.cmp(&d2)
-        } else {
-            a1.partial_cmp(&a2).unwrap()
+        let cw = ccw((x1 - x0, y1 - y0), (x2 - x0, y2 - y0));
+        match cw {
+            0 => {
+                let d1 = (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0);
+                let d2 = (x2 - x0) * (x2 - x0) + (y2 - y0) * (y2 - y0);
+                d1.cmp(&d2)
+            }
+            1 => Ordering::Less,
+            -1 => Ordering::Greater,
+            _ => panic!(),
         }
     });
 
@@ -91,6 +77,6 @@ fn test_graham_scan() {
         (3, 1),
     ];
 
-    let ret = graham_scan(&points);
+    let ret = graham_scan(points);
     assert_eq!(ret, vec![(1, 1), (3, 1), (3, 2), (2, 3), (1, 3)]);
 }
